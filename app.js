dbc7fc1c9e4588ee2d3d5cac1c38d9bc4feb50c0
tintapolipka <109940436+tintapolipka@users.noreply.github.com>
2023-05-27 18:42:34 +0200

    Original code simplified
    Using variables for texts;
    AI pseudocode added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const gameBoard = document.querySelector("#gameboard");
 const infoDisplay = document.getElementById("info");
 
 const startCells = ["", "", "", "", "", "", "", "", ""];
+let jatekVege = false;
 let go = "circle";
 infoDisplay.textContent = "Circle goes first";
 
@@ -33,9 +34,13 @@ function addGo(e) {
   e.target.append(goDisplay);
   
   checkScore();
-  go = go === "circle" ? "cross" : "circle";
+  if(!jatekVege)
+  {go = go === "circle" ? "cross" : "circle";
   infoDisplay.textContent = `It is now ${go}'s turn.`;
   e.target.removeEventListener("click", addGo);
+  if (playersTurn) {PlayerStep(e.target.id)};
+  playersTurn =  playersTurn? false:true;
+  }
   
 }
 
@@ -44,35 +49,99 @@ function checkScore() {
  
   winningCombos.forEach((array) => {
     const circleWins = array.every((cell) =>
-      allSquares[cell].firstElementChild?.classList.contains("circle")
+      allSquares[cell].firstElementChild?.classList.contains(go)
     );
 
+    console.log("circleWins: " + circleWins);
     if (circleWins) {
-      infoDisplay.textContent = "Circle Wins!";
+      infoDisplay.textContent = `${go==="circle"?"Circle":"Cross"} Wins!`;
       allSquares.forEach((square) =>
         square.replaceWith(square.cloneNode(true))
         
       );
+      jatekVege = true;
       return;
     }
   });
-
-  winningCombos.forEach((array) => {
-    const crossWins = array.every((cell) =>
-      allSquares[cell].firstElementChild?.classList.contains("cross")
-    );
-
-    if (crossWins) {
-      infoDisplay.textContent = "Cross Wins!";
-      allSquares.forEach((square) =>
-        square.replaceWith(square.cloneNode(true))
-        
-      );
-      return;
-    }
-  });
-
 }
+
+// AI
+let board = [0,0,0,0,0,0,0,0,0];
+let emptySpaces = [0,1,2,3,4,5,6,7,8];
+
+function AIstep(){/*Egyelőre nem működik*/
+  emptySpaces.forEach(isItWinning);
+  if(Boolean(squareToMark===0|squareToMark)){
+    //egyéb dolgok amiket meg kell tenni, ha van már célmező.
+    removeEmptySpace(squareToMark);
+    board[squareToMark] = -1;
+    printAIstep();
+    squareToMark = null;
+    return;
+  };
+
+  
+}
+  //a végül kiválasztandó mező:
+  let playersTurn = true;
+  let squareToMark;
+  //1. megvizsgálni, hogy lehet-e nyerni -melyik mezővel? Ott ahol már 2 az összeg.
+  //emptySpaces.forEach()
+
+  function isItWinning(spaceNr){
+    let relevantCombos = relevantWinningCombos(spaceNr);
+    let firstWinningOption = null;
+    relevantCombos.forEach(item => {if(comboReducer(item)===2){firstWinningOption = spaceNr}})
+    if(firstWinningOption){squareToMark=firstWinningOption};
+    console.log(squareToMark);
+  }  
+
+  function relevantWinningCombos(spaceNr){
+   return winningCombos.filter(item => item.includes(spaceNr));
+  }
+    //funkció, ami összeadja a board adott wininngCombojának megfelelő elemeit.
+  function comboReducer(currentCombo){
+    return currentCombo.reduce((accu,actu)=>{return accu + board[actu]},0)
+  }
+ 
+
+
+  //2. Megvizsgálni, hogy fenyeget-e a vereség -melyik mezőnél?
+  //3. Ha egyik se, akkor a legjobb mező kiválasztása
+
+  //AI lépésének megjelenítése:
+  function printAIstep() {
+    const goDisplay = document.createElement("div");
+    goDisplay.classList.add(go);
+    document.getElementById(squareToMark+"").append(goDisplay);
+    
+    checkScore();
+    if(!jatekVege)
+    {go = go === "circle" ? "cross" : "circle";
+    infoDisplay.textContent = `It is now ${go}'s turn.`;
+    document.getElementById(squareToMark+"").removeEventListener("click", addGo);
+    playersTurn =  playersTurn? false:true;
+    }
+    
+  }
+
+
+  //emptySpacesCallback
+  //removeEmptySpace(Kijelölt mező)
+  //board[kijelölt mező] = -1;
+
+
+
+
+function PlayerStep(target_id){
+  removeEmptySpace(+target_id);
+  board[target_id] = 1;
+}
+
+function removeEmptySpace(target_id){
+  let toDelete = [emptySpaces.indexOf(target_id)];
+  emptySpaces.splice(toDelete,1);
+};
 
 //start a new game
 crateBoard();
